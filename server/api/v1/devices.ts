8b803946629a9c9b99daebe6a77c880f8e1da08a
Ezra Lazuardy <ezralucio@gmail.com>
2022-04-01 03:46:40 +0700

    feat: enable prisma support

--- server/api/v1/devices.ts
+++ server/api/v1/devices.ts
@@ -1,10 +1,13 @@
 import type { IncomingMessage, ServerResponse } from "http";
 import { withHTTPMethod } from "~/helpers/useHTTPAction";
+import { usePrisma } from "~/helpers/usePrisma";
+
+const prisma = usePrisma();
 
 async function onGET(req: IncomingMessage, res: ServerResponse) {
-  return {
-    success: true,
-  };
+  return await prisma.device.findMany({
+    take: 10,
+  });
 }
 
 export default withHTTPMethod({ onGET });
