1d1628b149a5b69bcefc1ace8ff90aa2b9d83133
Ezra Lazuardy <ezralucio@gmail.com>
2022-04-05 08:59:26 +0700

    fix: cors error

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -5,22 +5,21 @@ export default async (req: IncomingMessage, res: ServerResponse) => {
   // allow CORS protocol on api endpoints
   if (req.url && !req.url.includes("v1")) return;
 
+  // set CORS headers
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Methods, Access-Control-Allow-Headers, X-Frame-Options, X-Requested-With, Origin, Content-Type, Accept, Api-Key, Device-Key, Authorization"
+  );
+  res.setHeader("X-Frame-Options", "ALLOWALL");
+
   // allow CORS preflight
   if (isMethod(req, "OPTIONS")) {
-    // set CORS headers
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Methods, Access-Control-Allow-Headers, X-Frame-Options, X-Requested-With, Origin, Content-Type, Accept, Api-Key, Device-Key, Authorization"
-    );
-    res.setHeader("X-Frame-Options", "ALLOWALL");
-
-    // ser CORS status code
     res.statusCode = 200;
     return res.end();
   }
