186958a7f1d6a8a93345cbd8aca62915b323c96c
Ezra Lazuardy <ezralucio@gmail.com>
2022-04-05 08:52:16 +0700

    fix: cors error

--- server/middleware/cors.ts
+++ server/middleware/cors.ts
@@ -9,13 +9,14 @@ export default async (req: IncomingMessage, res: ServerResponse) => {
   if (isMethod(req, "OPTIONS")) {
     // set CORS headers
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader(
       "Access-Control-Allow-Methods",
       "GET, POST, PUT, DELETE, OPTIONS"
     );
     res.setHeader(
       "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Api-Key, Device-Key, Authorization"
+      "Access-Control-Allow-Origin, Access-Control-Allow-Credentials, Access-Control-Allow-Methods, Access-Control-Allow-Headers, X-Frame-Options, X-Requested-With, Origin, Content-Type, Accept, Api-Key, Device-Key, Authorization"
     );
     res.setHeader("X-Frame-Options", "ALLOWALL");
 
