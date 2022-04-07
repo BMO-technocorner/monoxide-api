802e875ce869c9202a5ad4c27152ae85db1e1a30
Ezra Lazuardy <ezralucio@gmail.com>
2022-04-07 16:02:01 +0700

    fix: refactor api http method

--- helpers/prisma.ts
+++ helpers/prisma.ts
@@ -1,5 +1,10 @@
 import Prisma from "@prisma/client";
 
+let prisma;
+
 export type PrismaClient = Prisma.PrismaClient;
 
-export const usePrisma = (): PrismaClient => new Prisma.PrismaClient();
+export const usePrisma = (): PrismaClient => {
+  if (!prisma) prisma = new Prisma.PrismaClient();
+  return prisma;
+};
